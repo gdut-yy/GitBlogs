4237ecd423fb1347c4c9e0d4ca099c54dcddd22c
gdut-yy <gdut_yy@163.com>
2019-09-11 00:14:18 +0800

    118

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,6 +6,24 @@ module.exports = {
   description: '成长没有偏旁，所以它才孤独。',
   head: [['link', { rel: 'icon', href: '/favicon.png' }]],
   themeConfig: {
+    // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
+    repo: 'gdut-yy/GitBlogs',
+    // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
+    // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
+    repoLabel: 'Github',
+    // 以下为可选的编辑链接选项
+    // 假如你的文档仓库和项目本身不在一个仓库：
+    docsRepo: 'gdut-yy/GitBlogs',
+    // 假如文档不是放在仓库的根目录下：
+    // docsDir: 'docs',
+    // 假如文档放在一个特定的分支下：
+    docsBranch: 'github-io',
+    // 默认是 false, 设置为 true 来启用
+    editLinks: true,
+    // 默认为 "Edit this page"
+    editLinkText: '帮助我们改善此页面！',
+    // 最后更新时间
+    lastUpdated: 'Last Updated',
     // 导航栏
     nav: [
       { text: '首页', link: '/' },
@@ -33,16 +51,25 @@ module.exports = {
       {
         text: '阅读',
         items: [
-          { text: '校选教材', link: '/module_book/edu/' },
-          { text: '技术书籍', link: '/module_book/tech/' },
-          { text: '泛技术书籍', link: '/module_book/tech-extra/' }
+          {
+            text: 'Group1',
+            items: [
+              { text: '技术书籍', link: '/module_book/tech/' },
+              { text: '泛技术书籍', link: '/module_book/tech-extra/' }
+            ]
+          },
+          { text: 'Group2', items: [{ text: '校选教材', link: '/module_book/edu/' }] }
         ]
       },
       {
         text: '观影',
         items: [{ text: '电影', link: '/module_movie/movie/' }, { text: '电视剧', link: '/module_movie/tv/' }]
       },
-      { text: 'Github', link: 'https://www.github.com/gdut-yy' }
+      {
+        text: 'Blog',
+        items: [{ text: '源码学习', link: '/module_blog/' }, { text: '建设中', link: '/module_blog/' }]
+      }
+      // { text: 'Github', link: 'https://www.github.com/gdut-yy' }
     ],
     // 侧边栏
     sidebar: {
